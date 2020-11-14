bb188f62c0baa1b34b16650a8027f76fbfbd776c
kavan25 <kavanthakar1996@gmail.com>
2020-11-14 15:59:03 +0530

    Updates

---
+++ precache-manifest.8896f86411fce8211f0dcbac9fb8c2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7456535e0c437a9ff1af143f99d39d",
+    "url": "/ReactJS_Quiz_Application/index.html"
+  },
+  {
+    "revision": "174f0cf8a789a6724b50",
+    "url": "/ReactJS_Quiz_Application/static/css/main.afe3adcd.chunk.css"
+  },
+  {
+    "revision": "ba9eb1ddec68d2791129",
+    "url": "/ReactJS_Quiz_Application/static/js/2.56ccd3bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS_Quiz_Application/static/js/2.56ccd3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174f0cf8a789a6724b50",
+    "url": "/ReactJS_Quiz_Application/static/js/main.4c4b7d14.chunk.js"
+  },
+  {
+    "revision": "289a1b6c7f7bf470394e",
+    "url": "/ReactJS_Quiz_Application/static/js/runtime-main.9bd8d8fa.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactJS_Quiz_Application/precache-manifest.8896f86411fce8211f0dcbac9fb8c2ce.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactJS_Quiz_Application/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
